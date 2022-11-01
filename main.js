2660bcbbcb3f97a74de0a14a4e0f789c17544291
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-10-31 20:33:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var leftwristX = 0;
+var rightwristX = 0;
+var difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,12 +17,19 @@ function modelLoaded(){
     console.log("PoseNet is initialized");
 }
 
+function draw(){
+    background('burlywood');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be: " + difference + "px";
+    textSize(difference);
+    fill('black');
+    text("Hello", 50, 400);
+}
+
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
     }
-}
-
-function draw(){
-    background('burlywood');
 }
